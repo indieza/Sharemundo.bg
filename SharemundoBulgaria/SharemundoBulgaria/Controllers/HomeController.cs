4351858a8bd791ce6eb02f32c62fa49a63d3fbe5
indieza <simeonvalev00@gmail.com>
2020-09-16 10:21:27 +0300

    Connect Template for About Us Page

--- SharemundoBulgaria/SharemundoBulgaria/Controllers/HomeController.cs
+++ SharemundoBulgaria/SharemundoBulgaria/Controllers/HomeController.cs
@@ -11,19 +11,11 @@ namespace SharemundoBulgaria.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
