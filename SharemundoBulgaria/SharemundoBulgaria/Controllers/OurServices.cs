7669b9202d2b49843ef4f9c362f837b6e684bdc8
indieza <simeonvalev00@gmail.com>
2020-09-16 15:51:59 +0300

    Add "Our Services" and "Contact Us" pages design

---
+++ SharemundoBulgaria/SharemundoBulgaria/Controllers/OurServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharemundoBulgaria.Controllers
+{
+    public class OurServices : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
