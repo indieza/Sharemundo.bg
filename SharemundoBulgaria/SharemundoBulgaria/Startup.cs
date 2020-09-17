23c9b8e515723103dadf9f77b704d38588e0197d
indieza <simeonvalev00@gmail.com>
2020-09-17 10:32:43 +0300

    Create new Admin Area and import CSS, JS etc. files

--- SharemundoBulgaria/SharemundoBulgaria/Startup.cs
+++ SharemundoBulgaria/SharemundoBulgaria/Startup.cs
@@ -61,6 +61,10 @@ namespace SharemundoBulgaria
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                         name: "areas",
+                         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
