3765f6992ccb2d77a53258f8516f6986f7084476
indieza <simeonvalev00@gmail.com>
2020-10-21 17:22:20 +0300

    Work on admin panel

---
+++ SharemundoBulgaria/SharemundoBulgaria/Areas/Administration/Controllers/EditHomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace SharemundoBulgaria.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using SharemundoBulgaria.Constraints;
+
+    [Area(Constants.AdministrationArea)]
+    public class EditHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
