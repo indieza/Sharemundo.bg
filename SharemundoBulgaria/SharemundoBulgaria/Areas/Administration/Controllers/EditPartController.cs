4877673827b77043b7bb85a205a2cb49e92bc6d1
indieza <simeonvalev00@gmail.com>
2020-10-28 22:53:45 +0200

    Make the edit pages

---
+++ SharemundoBulgaria/SharemundoBulgaria/Areas/Administration/Controllers/EditPartController.cs
@@ -0,0 +1,20 @@
+﻿namespace SharemundoBulgaria.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using SharemundoBulgaria.Constraints;
+
+    [Area(Constants.AdministrationArea)]
+    [Authorize(Roles = Constants.AdministratorRole)]
+    public class EditPartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
