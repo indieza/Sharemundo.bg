311178f44221df59f3576edabbd822a1eac320a9
indieza <simeonvalev00@gmail.com>
2020-10-30 11:24:53 +0200

    Implement JS logic for changing elements position

--- SharemundoBulgaria/SharemundoBulgaria/wwwroot/administration/js/editPartsPositionScript.js
+++ SharemundoBulgaria/SharemundoBulgaria/wwwroot/administration/js/editPartsPositionScript.js
@@ -24,15 +24,38 @@
                             <input class="form-control" type="text" placeholder="Name" value="${part.name}" disabled/>
                         </div>
                         <div class="form-group col-lg-3">
-                            <input class="form-control" type="number" placeholder="0"  value="${part.positionNumber}" />
+                            <input class="form-control" type="number" min="1" placeholder="0"  value="${part.positionNumber}" onchange="positionChange(this)"/>
                         </div>
                         <input type="hidden" value="${part.id}" />
                     </div>
                     <hr  style="border-top: 1px solid grey; border-radius: 5px;"/>`;
+            }
+
+            if (data.length > 0) {
+                div.innerHTML += `
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <button class="btn btn-info" href="#">
+                                Edit
+                            </button>
+                        </div>
+                    </div>`;
             }
         },
         error: function (msg) {
             console.error(msg);
         }
     })
+}
+
+function positionChange(currentInput) {
+    let allNubersInputFields = document.querySelectorAll('input[type=number]');
+
+    for (var currentInputNumber of allNubersInputFields) {
+        if (currentInput != currentInputNumber && currentInput.value == currentInputNumber.value) {
+            currentInputNumber.value = currentInput.defaultValue;
+            currentInputNumber.defaultValue = currentInputNumber.value;
+        }
+    }
+    currentInput.defaultValue = currentInput.value;
 }
--- SharemundoBulgaria/SharemundoBulgaria/wwwroot/administration/js/editSectionsPositionScript.js
+++ SharemundoBulgaria/SharemundoBulgaria/wwwroot/administration/js/editSectionsPositionScript.js
@@ -24,15 +24,38 @@
                             <input class="form-control" type="text" placeholder="Name" value="${section.name}" disabled/>
                         </div>
                         <div class="form-group col-lg-3">
-                            <input class="form-control" type="number" placeholder="0"  value="${section.positionNumber}" />
+                            <input class="form-control" type="number" min="1" placeholder="0"  value="${section.positionNumber}"  onchange="positionChange(this)"/>
                         </div>
                         <input type="hidden" value="${section.id}" />
                     </div>
                     <hr  style="border-top: 1px solid green; border-radius: 5px;"/>`;
+            }
+
+            if (data.length > 0) {
+                div.innerHTML += `
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <button class="btn btn-info" href="#">
+                                Edit
+                            </button>
+                        </div>
+                    </div>`;
             }
         },
         error: function (msg) {
             console.error(msg);
         }
     })
+}
+
+function positionChange(currentInput) {
+    let allNubersInputFields = document.querySelectorAll('input[type=number]');
+
+    for (var currentInputNumber of allNubersInputFields) {
+        if (currentInput != currentInputNumber && currentInput.value == currentInputNumber.value) {
+            currentInputNumber.value = currentInput.defaultValue;
+            currentInputNumber.defaultValue = currentInputNumber.value;
+        }
+    }
+    currentInput.defaultValue = currentInput.value;
 }
